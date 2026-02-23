5e9692f055dda60c900169267453b3ca9a246275
abeni-hub <ebenezarsileshi@gmail.com>
2026-02-23 10:18:04 +0300

    update the above error

--- expenses/services.py
+++ expenses/services.py
@@ -2,7 +2,6 @@ from decimal import Decimal
 from django.db.models import F
 from .models import Account
 
-
 def get_account(account_type):
     account, _ = Account.objects.get_or_create(
         account_type=account_type,
@@ -10,14 +9,28 @@ def get_account(account_type):
     )
     return account
 
+def apply_expense(amount, payment_source, amount_cash=0, amount_bank=0):
+    if payment_source == 'combined':
+        cash = get_account('cash')
+        bank = get_account('bank')
+        cash.balance = F('balance') - Decimal(amount_cash)
+        bank.balance = F('balance') - Decimal(amount_bank)
+        cash.save(update_fields=['balance'])
+        bank.save(update_fields=['balance'])
+    else:
+        account = get_account(payment_source)
+        account.balance = F('balance') - Decimal(amount)
+        account.save(update_fields=['balance'])
 
-def apply_expense(amount, payment_source):
-    account = get_account(payment_source)
-    account.balance = F('balance') - Decimal(amount)
-    account.save(update_fields=['balance'])
-
-
-def rollback_expense(amount, payment_source):
-    account = get_account(payment_source)
-    account.balance = F('balance') + Decimal(amount)
-    account.save(update_fields=['balance'])
+def rollback_expense(amount, payment_source, amount_cash=0, amount_bank=0):
+    if payment_source == 'combined':
+        cash = get_account('cash')
+        bank = get_account('bank')
+        cash.balance = F('balance') + Decimal(amount_cash)
+        bank.balance = F('balance') + Decimal(amount_bank)
+        cash.save(update_fields=['balance'])
+        bank.save(update_fields=['balance'])
+    else:
+        account = get_account(payment_source)
+        account.balance = F('balance') + Decimal(amount)
+        account.save(update_fields=['balance'])

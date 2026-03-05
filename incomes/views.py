32ee8209bedcfd5988ceffac878fdc158f5b9311
abeni-hub <ebenezarsileshi@gmail.com>
2026-03-05 14:26:39 +0300

    update seralizers and views

--- incomes/views.py
+++ incomes/views.py
@@ -2,54 +2,103 @@ from django.db import transaction
 from rest_framework import viewsets
 from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.exceptions import ValidationError
 
 from .models import Income
 from .serializers import IncomeSerializer
-from expenses.services import apply_income, rollback_income   # we'll update services.py below
+
+from expenses.services import apply_income, rollback_income
+
 
 class IncomeViewSet(viewsets.ModelViewSet):
-    queryset = Income.objects.all()
+
+    queryset = Income.objects.all().order_by('-date')
     serializer_class = IncomeSerializer
 
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
-    filterset_fields = ['category', 'payment_source', 'date']
-    search_fields = ['description', 'remarks']
-    ordering_fields = ['date', 'amount']
 
+    # Filtering
+    filterset_fields = [
+        'transaction_type',
+        'category',
+        'payment_source',
+        'status',
+        'date'
+    ]
+
+    # Search
+    search_fields = [
+        'description',
+        'payer_name',
+        'reference_number',
+        'remarks'
+    ]
+
+    # Ordering
+    ordering_fields = [
+        'date',
+        'amount',
+        'amount_paid',
+        'created_at'
+    ]
+
+    # ----------------------------
+    # CREATE INCOME
+    # ----------------------------
     @transaction.atomic
     def perform_create(self, serializer):
-        income = serializer.save()
-        apply_income(
-            income.amount,
-            income.payment_source,
-            income.amount_cash or 0,
-            income.amount_bank or 0
-        )
 
+        income = serializer.save()
+
+        # Only apply paid money to balance
+        if income.amount_paid > 0:
+            apply_income(
+                income.amount_paid,
+                income.payment_source,
+                income.amount_cash or 0,
+                income.amount_bank or 0
+            )
+
+    # ----------------------------
+    # UPDATE INCOME
+    # ----------------------------
     @transaction.atomic
     def perform_update(self, serializer):
-        old_income = self.get_object()
-        rollback_income(
-            old_income.amount,
-            old_income.payment_source,
-            old_income.amount_cash or 0,
-            old_income.amount_bank or 0
-        )
-        income = serializer.save()
-        apply_income(
-            income.amount,
-            income.payment_source,
-            income.amount_cash or 0,
-            income.amount_bank or 0
-        )
 
+        old_income = self.get_object()
+
+        # rollback previous paid money
+        if old_income.amount_paid > 0:
+            rollback_income(
+                old_income.amount_paid,
+                old_income.payment_source,
+                old_income.amount_cash or 0,
+                old_income.amount_bank or 0
+            )
+
+        income = serializer.save()
+
+        # apply new payment
+        if income.amount_paid > 0:
+            apply_income(
+                income.amount_paid,
+                income.payment_source,
+                income.amount_cash or 0,
+                income.amount_bank or 0
+            )
+
+    # ----------------------------
+    # DELETE INCOME
+    # ----------------------------
     @transaction.atomic
     def perform_destroy(self, instance):
-        rollback_income(
-            instance.amount,
-            instance.payment_source,
-            instance.amount_cash or 0,
-            instance.amount_bank or 0
-        )
+
+        # rollback paid money
+        if instance.amount_paid > 0:
+            rollback_income(
+                instance.amount_paid,
+                instance.payment_source,
+                instance.amount_cash or 0,
+                instance.amount_bank or 0
+            )
+
         instance.delete()
